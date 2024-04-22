cd838015b0a8568abec1756334a4a7e4eb631e16
ninadparange <127338481+ninadparange@users.noreply.github.com>
2024-04-22 20:22:46 +0530

    Add files via upload

---
+++ TheKalamart/Website/paytmk/include/nav.php
@@ -0,0 +1,13 @@
+ <center> 
+        <nav class="navbar">
+          <a class="navbar-brand " href="kart.php">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Paytm_logo.jpg" width="100" height="30" class="d-inline-block align-top" alt="">
+             Payment Intigration 
+          </a>
+           <ul class="nav navbar-nav navbar-right">
+
+		      <li> <a href="https://www.youtube.com/channel/UCArlD6o-JXBpQipSAXnvnFg"> <strong><i class="fab fa-youtube text-danger" ></i> thekalamart</strong></a></li>
+			  <li><a href="https://www.instagram.com/thekalamart_"> <strong><i class="fab fa-instagram text-default" ></i> instagram.com</a></li>
+		    </ul>
+        </nav>
+    </center>
